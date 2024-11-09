acbcfcdf4092921ff931bc213ea01b118ad03254
sam1234566745 <sammepaul101@gmail.com>
2024-11-09 12:10:33 +0530

    Create hu.sh

---
+++ hu.sh
@@ -0,0 +1,7 @@
+echo "# hu" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sam1234566745/hu.git
+git push -u origin main
